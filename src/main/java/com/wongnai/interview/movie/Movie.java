5b890a7801f496249645fd139c325ab1950c6c03
Kawisara Bunyen <zelotype.3@gmail.com>
2020-02-14 00:27:08 +0700

    Complete second

--- src/main/java/com/wongnai/interview/movie/Movie.java
+++ src/main/java/com/wongnai/interview/movie/Movie.java
@@ -27,8 +27,9 @@ public class Movie {
 	protected Movie() {
 	}
 
-	public Movie(String name) {
+	public Movie(String name, List<String> actors) {
 		this.name = name;
+		this.actors = actors;
 	}
 
 	public Long getId() {
--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,7 +1,10 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import com.wongnai.interview.movie.external.MovieData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +22,19 @@ public class SimpleMovieSearchService implements MovieSearchService {
 		//TODO: Step 2 => Implement this method by using data from MovieDataService
 		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
 		// Please do not change @Component annotation on this class
-		return null;
+
+		List<Movie> matchedMovies = new ArrayList<Movie>();
+		List<MovieData> movies = movieDataService.fetchAll();
+
+		movies.forEach(movie -> {
+			List<String> splitedTitle = Arrays.asList(movie.getTitle().split(" "));
+			splitedTitle.forEach(title -> {
+				if(title.equalsIgnoreCase(queryText)){
+					matchedMovies.add(new Movie(movie.getTitle(), movie.getCast()));
+				}
+			});
+		});
+
+		return matchedMovies;
 	}
 }
